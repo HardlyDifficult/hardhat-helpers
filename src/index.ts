4f1fe5d3da099b83e415641edf97a9dec71c9da4
NickCuso <hardlydiff@gmail.com>
2022-08-07 13:46:28 -0400

    Forked DAI

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from "./expectAllEvents";
 export * from "./balance";
 export * from "./fork";
+export * as mainnet from "./mainnet";
+export * from "./typechain";
 export * from "./snapshotEach";
 export * from "./storage";
 export * from "./time";
