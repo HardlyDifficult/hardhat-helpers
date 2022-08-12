2edddf4e8c51ee15180ea4ca2eea4202a0a1b833
NickCuso <hardlydiff@gmail.com>
2022-08-12 03:47:06 -0400

    Refactor snapshot each

--- src/snapshotEach.ts
+++ src/snapshotEach.ts
@@ -4,17 +4,17 @@ import { Context } from "mocha";
 export function snapshotEach(funcBeforeSnapshot: (this: Context) => Promise<void>): void {
   let snapshotId: string;
 
-  before(async function () {
-    await funcBeforeSnapshot.call(this);
-    snapshotId = await hre.network.provider.send("evm_snapshot", []);
-  });
-
   beforeEach(async function () {
-    await hre.network.provider.send("evm_revert", [snapshotId]);
+    if (!snapshotId) {
+      // First run only
+      await funcBeforeSnapshot.call(this);
+    }
+
+    // A new snapshot is required after each revert
     snapshotId = await hre.network.provider.send("evm_snapshot", []);
   });
 
-  after(async function () {
+  afterEach(async function () {
     // Clean up state when tests finish
     await hre.network.provider.send("evm_revert", [snapshotId]);
   });
