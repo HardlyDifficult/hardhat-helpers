ed4f1763642cdb6e5b836562edee9cc22d5bc15f
NickCuso <hardlydiff@gmail.com>
2022-08-14 05:59:34 -0400

    Tracer names

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export * from "./typechain";
 export * from "./snapshotEach";
 export * from "./storage";
 export * from "./time";
+export * from "./tracer";
---
+++ src/tracer.ts
@@ -0,0 +1,18 @@
+import "hardhat-tracer";
+import { tracer } from "hardhat";
+
+export async function registerTracerNames<T extends { [key: string]: { address: string } }>(
+  accounts: T,
+  prefix?: string
+) {
+  // Add name aliases to tracer (e.g. when using --logs)
+  let accountName: keyof typeof accounts;
+  for (accountName in accounts) {
+    const signer = accounts[accountName];
+    let name: string = accountName;
+    if (prefix) {
+      name = prefix + name;
+    }
+    tracer.nameTags[signer.address] = name;
+  }
+}
