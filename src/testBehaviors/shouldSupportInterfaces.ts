9f5a8f76df22aee32da527b4326dc865c5b249bb
HardlyDifficult <hardlydiff@gmail.com>
2022-08-26 07:51:30 -0400

    shouldSupportInterfaces
    WIP

---
+++ src/testBehaviors/shouldSupportInterfaces.ts
@@ -0,0 +1,105 @@
+// Originally from https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts-upgradeable/master/test/introspection/SupportsInterface.behavior.js
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+const INTERFACES: {[key: string]: string[]} = {
+  ERC165: ["supportsInterface(bytes4)"],
+  ERC721: [
+    "balanceOf(address)",
+    "ownerOf(uint256)",
+    "approve(address,uint256)",
+    "getApproved(uint256)",
+    "setApprovalForAll(address,bool)",
+    "isApprovedForAll(address,address)",
+    "transferFrom(address,address,uint256)",
+    "safeTransferFrom(address,address,uint256)",
+    "safeTransferFrom(address,address,uint256,bytes)",
+  ],
+  ERC721Enumerable: ["totalSupply()", "tokenOfOwnerByIndex(address,uint256)", "tokenByIndex(uint256)"],
+  ERC721Metadata: ["name()", "symbol()", "tokenURI(uint256)"],
+  ERC1155: [
+    "balanceOf(address,uint256)",
+    "balanceOfBatch(address[],uint256[])",
+    "setApprovalForAll(address,bool)",
+    "isApprovedForAll(address,address)",
+    "safeTransferFrom(address,address,uint256,uint256,bytes)",
+    "safeBatchTransferFrom(address,address,uint256[],uint256[],bytes)",
+  ],
+  ERC1155Receiver: [
+    "onERC1155Received(address,address,uint256,uint256,bytes)",
+    "onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)",
+  ],
+  ERC2981: ["royaltyInfo(uint256,uint256)"],
+};
+
+export const INTERFACE_IDS: any = {};
+const FN_SIGNATURES: any = {};
+for (const k of Object.getOwnPropertyNames(INTERFACES)) {
+  INTERFACE_IDS[k] = makeInterfaceId(INTERFACES[k]);
+  for (const fnName of INTERFACES[k]) {
+    // the interface id of a single function is equivalent to its function signature
+    FN_SIGNATURES[fnName] = makeInterfaceId([fnName]);
+  }
+}
+
+export function shouldSupportInterfaces(interfaces: (keyof typeof INTERFACES)[], supportedButNotRegistered = false): void {
+  describe("Contract interface", function () {
+    beforeEach(function () {
+      this.contractUnderTest = this.mock || this.token || this.holder || this.nft;
+    });
+
+    for (const interfaceName of interfaces) {
+      if(typeof interfaceName === "number") {
+        throw new Error("interfaces should be strings");
+      }
+
+      const interfaceId = INTERFACE_IDS[interfaceName];
+      describe(interfaceName, function () {
+        describe("ERC165's supportsInterface(bytes4)", function () {
+          it("uses less than 32k gas [skip-on-coverage]", async function () {
+            expect(await this.contractUnderTest.estimateGas.supportsInterface(interfaceId)).to.be.lte(32000);
+          });
+
+          it(
+            (supportedButNotRegistered ? "does not claim support" : "claims support") + ` for ${interfaceName} (${interfaceId})`,
+            async function () {
+              if (supportedButNotRegistered) {
+                expect(await this.contractUnderTest.supportsInterface(interfaceId)).to.equal(false);
+              } else {
+                expect(await this.contractUnderTest.supportsInterface(interfaceId)).to.equal(true);
+              }
+            },
+          );
+        });
+
+        for (const fnName of INTERFACES[interfaceName]) {
+          describe(fnName, function () {
+            it("has to be implemented", function () {
+              expect(this.contractUnderTest.functions[fnName]).to.be.not.null;
+            });
+          });
+        }
+      });
+    }
+  });
+}
+
+function makeInterfaceId (functionSignatures: string[]): string {
+  const INTERFACE_ID_LENGTH = 4;
+
+  const interfaceIdBuffer = functionSignatures
+    .map(signature => ethers.utils.keccak256(signature))
+    .map(h =>
+      Buffer
+        .from(h.substring(2), 'hex')
+        .slice(0, 4) // bytes4()
+    )
+    .reduce((memo, bytes) => {
+      for (let i = 0; i < INTERFACE_ID_LENGTH; i++) {
+        memo[i] = memo[i] ^ bytes[i]; // xor
+      }
+      return memo;
+    }, Buffer.alloc(INTERFACE_ID_LENGTH));
+
+  return `0x${interfaceIdBuffer.toString('hex')}`;
+}
---
+++ test/tests/testBehaviors/shouldSupportInterfaces.ts
@@ -0,0 +1,8 @@
+import { snapshotEach } from "../../../src"
+
+describe("testBehaviors / shouldSupportInterfaces", () => {
+    
+    snapshotEach(async () => {
+
+    })
+})
