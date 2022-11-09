0bf1d43006e10309b0dc7cdadba512fba759e133
HardlyDifficult <hardlydiff@gmail.com>
2022-11-09 10:51:30 -0500

    Calc share

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./create2";
 export * from "./events";
 export * from "./fork";
 export * as mainnet from "./mainnet";
+export * from "./math";
 export * from "./snapshotEach";
 export * from "./storage";
 export * from "./testBehaviors";
