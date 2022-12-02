067483229c57ce3cfe636c182f27ec1a1c6c3b47
HardlyDifficult <hardlydiff@gmail.com>
2022-12-02 10:03:09 -0500

    getRandomAddress BigNumberish params (#183)

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,19 +1,20 @@
+import { BigNumberish } from "ethers";
 import { ethers } from "hardhat";
 
 /**
  * Returns a pseudo-random address based on the index provided.
  */
-export function getRandomAddress(index: number) {
+export function getRandomAddress(index: BigNumberish) {
   return getRandomSigner(index).address;
 }
 
 /**
  * Returns a pseudo-random account based on the index provided.
  */
-export function getRandomSigner(index: number) {
+export function getRandomSigner(index: BigNumberish) {
   let wallet = ethers.Wallet.fromMnemonic(
     "test test test test test test test test test test test junk",
-    `m/44'/60'/0'/1/${index}`
+    `m/44'/60'/0'/1/${index.toString()}`
   );
   wallet = wallet.connect(ethers.provider);
   return wallet;
