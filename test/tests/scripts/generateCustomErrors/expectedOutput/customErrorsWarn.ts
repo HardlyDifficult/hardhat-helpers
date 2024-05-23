abc922f923ce42b9f65cda767a8838d057b8c122
HardlyDifficult <hardlydiff@gmail.com>
2024-05-23 15:42:49 -0400

    require error reasons

---
+++ test/tests/scripts/generateCustomErrors/expectedOutput/customErrorsWarn.ts
@@ -0,0 +1,98 @@
+export type CustomContractError = {
+  // Note: If there same error appears in multiple contracts, this field will be set to the first instance found.
+  contractName: string;
+  name: string;
+  errorCode: string;
+  params?: readonly {
+    name: string;
+    type: string;
+  }[];
+  reason: string;
+  description: string;
+};
+
+export const ContractErrorsByName = {
+  CustomErrors_Test_1: {
+    contractName: "CustomErrors",
+    name: "CustomErrors_Test_1",
+    errorCode: "0x730b17c8",
+    reason: "Test custom error 1",
+    description: "This is a test function to check the output of custom error 1",
+  },
+  CustomErrors_Test_2: {
+    contractName: "CustomErrors",
+    name: "CustomErrors_Test_2",
+    errorCode: "0x11b1f88a",
+    params: [
+      { name: "a", type: "uint256" },
+    ],
+    reason: "",
+    description: "",
+  },
+  CustomErrors_Test_3: {
+    contractName: "CustomErrors",
+    name: "CustomErrors_Test_3",
+    errorCode: "0xd3c4fb90",
+    params: [
+      { name: "a", type: "uint256" },
+      { name: "b", type: "uint256" },
+    ],
+    reason: "",
+    description: "",
+  },
+  CustomErrors_Test_4: {
+    contractName: "CustomErrors2",
+    name: "CustomErrors_Test_4",
+    errorCode: "0xa2e67f32",
+    params: [
+      { name: "a", type: "uint256" },
+      { name: "b", type: "uint256" },
+      { name: "c", type: "uint256" },
+    ],
+    reason: "",
+    description: "",
+  },
+} as const;
+
+export const ContractErrorsBySignature = {
+  "0x730b17c8": {
+    contractName: "CustomErrors",
+    name: "CustomErrors_Test_1",
+    errorCode: "0x730b17c8",
+    reason: "Test custom error 1",
+    description: "This is a test function to check the output of custom error 1",
+  },
+  "0x11b1f88a": {
+    contractName: "CustomErrors",
+    name: "CustomErrors_Test_2",
+    errorCode: "0x11b1f88a",
+    params: [
+      { name: "a", type: "uint256" },
+    ],
+    reason: "",
+    description: "",
+  },
+  "0xd3c4fb90": {
+    contractName: "CustomErrors",
+    name: "CustomErrors_Test_3",
+    errorCode: "0xd3c4fb90",
+    params: [
+      { name: "a", type: "uint256" },
+      { name: "b", type: "uint256" },
+    ],
+    reason: "",
+    description: "",
+  },
+  "0xa2e67f32": {
+    contractName: "CustomErrors2",
+    name: "CustomErrors_Test_4",
+    errorCode: "0xa2e67f32",
+    params: [
+      { name: "a", type: "uint256" },
+      { name: "b", type: "uint256" },
+      { name: "c", type: "uint256" },
+    ],
+    reason: "",
+    description: "",
+  },
+} as const;
--- test/tests/scripts/generateCustomErrors/generateErrors.test.ts
+++ test/tests/scripts/generateCustomErrors/generateErrors.test.ts
@@ -5,11 +5,22 @@ import { generateCustomErrorsFile } from "../../../../src/scripts";
 import { CustomErrors__factory, CustomErrors2__factory, CustomErrorsWithConflict__factory } from "../../../typechain";
 
 describe("scripts / generateCustomErrors / generateErrors", () => {
+  const contracts = [CustomErrors__factory, CustomErrors2__factory];
+
   it("Generate errors file", async () => {
-    const contracts = [CustomErrors__factory, CustomErrors2__factory];
     const results = generateCustomErrorsFile(contracts);
     const expected = readFileSync(`${__dirname}/expectedOutput/customErrors.ts`, "utf8");
     expect(results).to.eq(expected);
+  });
+
+  it("Generate errors file with placeholder", async () => {
+    const results = generateCustomErrorsFile(contracts, { reasonRequirement: "Warn", descriptionRequirement: "Warn"});
+    const expected = readFileSync(`${__dirname}/expectedOutput/customErrorsWarn.ts`, "utf8");
+    expect(results).to.eq(expected);
+  });
+
+  it("Generate errors fails with missing requirement", async () => {
+    expect(() => generateCustomErrorsFile(contracts, { reasonRequirement: "Error", descriptionRequirement: "Error"})).to.be.throw("Reason is required for error: CustomErrors_Test_2");    
   });
 
   describe("Conflict", () => {
